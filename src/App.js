a362367b43382cdc30aebb97cc89cd988143dd8c
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2023-03-15 14:40:11 +0100

    responsive touch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./mediaq.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import NoPage from "./components/NoPage";
+import ActionPage from "./components/ActionPage";
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           <Route path="login" element={<Login />} />
 
           <Route path="signup" element={<Signup />} />
+
 
           <Route path="*" element={<NoPage />} />
         </Routes>
